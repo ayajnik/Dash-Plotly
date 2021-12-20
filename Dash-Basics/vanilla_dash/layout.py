af1d97fc61b93305f4e74f12c230f23d46869f03
ayajnik <ayush.yajnik@yahoo.com>
2021-12-20 17:39:22 -0500

    vanilla dash dashboard

--- Dash-Basics/vanilla_dash/layout.py
+++ Dash-Basics/vanilla_dash/layout.py
@@ -1,11 +1,23 @@
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc
+from dash import html
 
 app = dash.Dash()
 
-app.layout(children=[
-    html.H1("Welcome to Dash")
+app.layout = html.Div(children=[
+    html.H1("Welcome to Dash"),
+     html.Div(children='Dash: A web application framework for Python.'),
+
+     dcc.Graph(
+         id = "My first Graph",
+         figure={
+             "data":[
+                 {'x':['1','2','3'], 'y':['4','5','6'],'type':'bar','name':'SF'},
+                 {'y':['2','3','4'], 'y':['5','6','7'], 'type':'bar','name':"NYC"}
+             ],
+             "layout": {"title":"My first Dash plot"}
+         }
+     )
 ])
 
 if __name__ =="__main__":
