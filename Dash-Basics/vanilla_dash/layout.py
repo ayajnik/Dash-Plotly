083bed587b82ceb933854d1b45653ae6c9413498
ayajnik <ayush.yajnik@yahoo.com>
2021-12-20 17:24:02 -0500

    dash and requirements.txt update

---
+++ Dash-Basics/vanilla_dash/layout.py
@@ -0,0 +1,12 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+app = dash.Dash()
+
+app.layout(children=[
+    html.H1("Welcome to Dash")
+])
+
+if __name__ =="__main__":
+    app.run_server()
